6eb01818294a984791a9d7081d8f3260168f7d80
code4mber <amberj_steen@outlook.com>
2024-02-26 16:05:49 +1100

    updated context

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ event.preventDefault();
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "552d5b0d84b3fo63f5d841c0ct5a8338";
 let prompt = `User instructions are: Generate a short fictional story about ${instructionsInput.value}`;
-let context =
-  "You are a creative writting expert and love to write short stories. Your mission is to generate a single paragraph short story. Make sure to follow the user instructions. Sign the story with '- SheCodes AI' inside a <strong></strong> element at the end.";
+let context = "You are a creative writting expert and love to write short stories. Your mission is to generate a single paragraph short story. Make sure to follow the user instructions. Sign the story with '- SheCodes AI' inside a <strong></strong> element at the end.";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let storyElement = document.querySelector("#story");
