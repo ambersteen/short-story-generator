50921c9496ac494bb1f509c742166cd8a35b2e1e
code4mber <amberj_steen@outlook.com>
2024-02-26 15:17:32 +1100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -14,9 +14,15 @@ event.preventDefault();
 
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "552d5b0d84b3fo63f5d841c0ct5a8338";
-let prompt = `User instructions are: Generate a short story about ${instructionsInput.value}`;
-let context = "You are a fiction story expert and love to write short stories. Your mission is to generate a single paragraph short story. Make sure to follow the user instructions. Sign the story with '- SheCodes AI' inside a <strong></strong> element at the end.";
+let prompt = `User instructions are: Generate a short fictional story about ${instructionsInput.value}`;
+let context =
+  "You are a creative writting expert and love to write short stories. Your mission is to generate a single paragraph short story. Make sure to follow the user instructions. Sign the story with '- SheCodes AI' inside a <strong></strong> element at the end.";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let storyElement = document.querySelector("#story");
+storyElement.classList.remove("hidden");
+storyElement.innerHTML =
+  `Generating story for you about ${instructionsInput.value}`;
 
 console.log("generating story");
 console.log(`prompt: ${prompt}`);
